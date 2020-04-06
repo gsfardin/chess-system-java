c1d81ea690555bdbd8552faee4dc8c6bc1e103fa
Gabriel Fardin <gsfardin@gmail.com>
2020-04-06 14:39:34 -0300

    Fist class: Position

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,11 +1,15 @@
 package apllication;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		System.out.println("Begin chess system project");
-
+		
+		Position position = new Position(1, 2);
+		System.out.println(position);
 	}
 
 }
